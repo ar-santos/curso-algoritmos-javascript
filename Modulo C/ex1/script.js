ba6b19fb3fd0c19a52fc9d7424871854e2f7f235
ar-santos <arthursantos4367@gmail.com>
2025-02-17 15:45:10 -0300

    Módulo C

---
+++ Modulo C/ex1/script.js
@@ -0,0 +1,11 @@
+var p1 = document.getElementsByTagName('p')[0];
+var corpo = document.body;
+var mensagem = document.getElementById('msg')
+
+// p1.style.color = 'black';
+// corpo.style.background = 'black';
+
+mensagem.style.background = 'green'
+// mensagem.innerText = 'Estou aguardando...'
+
+document.write('Está escrito assim: ' + p1.innerText);
